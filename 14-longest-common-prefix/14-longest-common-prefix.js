f8155c180f4cbc00e83cdfd691ced1767cfead18
Noah Thomlison <NoahThomlison@gmail.com>
2022-04-23 14:51:55 -0700

    Time: 250 ms (5.18%), Space: 50.2 MB (5.08%) - LeetHub

---
+++ 14-longest-common-prefix/14-longest-common-prefix.js
@@ -0,0 +1,33 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+ var longestCommonPrefix = function(strs) {
+  let commonPrefix = ""
+  let commonLetter = ""
+  let uncommonLetter = false
+  let i = 0
+  while (uncommonLetter === false){
+    for(let j = 0; j < strs.length; j++){
+      console.log(`letter: ${strs[j][i]}`)
+      if(strs[j][i] === undefined){
+        uncommonLetter = true
+        break
+      }
+      if(j == 0){
+        commonLetter = strs[j][i]
+        console.log(`commonLetter: ${commonLetter}`)
+      }
+      if(strs[j][i] !== commonLetter){
+        uncommonLetter = true
+        break
+      }
+      if(j === strs.length-1){
+        commonPrefix += commonLetter
+      }
+    }
+    console.log(`commonPrefix: ${commonPrefix}`)
+    i++
+  }
+  return(commonPrefix)
+};
