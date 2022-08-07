9b1a805624dda191a00cf24e64f50376ae28b9eb
Noah Thomlison <NoahThomlison@gmail.com>
2022-08-07 12:11:45 -0700

    Time: 140 ms (9.48%), Space: 47.1 MB (5.20%) - LeetHub

--- 169-majority-element/169-majority-element.js
+++ 169-majority-element/169-majority-element.js
@@ -1,26 +1,25 @@
+
 /**
  * @param {number[]} nums
  * @return {number}
  */
  var majorityElement = function(nums) {
-  let numberToCheck 
-  let occurances
-  let highestOccurances = 0
-  let majorityNumber
-    for(let i = 0; i < nums.length; i++){
-      occurances = 1
-      numberToCheck = nums[i]
-      for(let j = i + 1; j < nums.length; j++){
-        if(numberToCheck === nums[j]){
-          occurances++
-          nums.splice(j, 1)
-          j--
-        }
-      }
-      if(occurances > highestOccurances){
-        highestOccurances = occurances
-        majorityNumber = numberToCheck
+  nums.sort()
+  let count = 0
+  let highestCount = 0
+  let mostCommon = nums[0]
+  console.log(nums)
+  for(let i = 0; i < nums.length; i++){
+    if(nums[i] === nums[i+1]){
+       count++
+       if(count > highestCount){
+        highestCount = count
+        mostCommon = nums[i]
       }
     }
-    return majorityNumber
+    else{
+      count = 0
+    }
+  }
+  return(mostCommon)
 };
