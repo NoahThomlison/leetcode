62ba24b071c4f067ee7addde777bc185fe63073c
Noah Thomlison <NoahThomlison@gmail.com>
2022-08-08 21:47:02 -0700

    Time: 165 ms (24.03%), Space: 49.5 MB (38.86%) - LeetHub

--- 344-reverse-string/344-reverse-string.js
+++ 344-reverse-string/344-reverse-string.js
@@ -6,6 +6,10 @@
   let t = s.length - 1
   for(let i = 0; i < s.length/2 ; i++){
     let temp = s[i]
+    if(s[i] === s[t]){
+      t--
+      continue
+    }
     s[i] = s[t]
     s[t] = temp
     t--
