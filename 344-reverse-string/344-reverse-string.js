42f4c7914b623fb645b8dd8d094d8367e86b6cad
Noah Thomlison <NoahThomlison@gmail.com>
2022-08-08 21:44:05 -0700

    Time: 166 ms (23.39%), Space: 49.5 MB (38.86%) - LeetHub

--- 344-reverse-string/344-reverse-string.js
+++ 344-reverse-string/344-reverse-string.js
@@ -3,5 +3,12 @@
  * @return {void} Do not return anything, modify s in-place instead.
  */
  var reverseString = function(s) {
-    return(s.reverse())
+  let t = s.length - 1
+  for(let i = 0; i < s.length/2 ; i++){
+    let temp = s[i]
+    s[i] = s[t]
+    s[t] = temp
+    t--
+  }
+  return(s)
 };
