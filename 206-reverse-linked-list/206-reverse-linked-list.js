2cce3e49c9623fcc2b393b03a2a08d40fa2f63e7
Noah Thomlison <NoahThomlison@gmail.com>
2022-08-13 14:12:17 -0700

    Time: 84 ms (69.58%), Space: 44.6 MB (23.47%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.js
+++ 206-reverse-linked-list/206-reverse-linked-list.js
@@ -12,14 +12,18 @@
 var reverseList = function(head) {
     let current = head
     let prev = null
-    let next;
+    let next
     
-    while(current !== null) {
+    while(current){
         next = current.next
         current.next = prev
         prev = current
         current = next
     }
     
-    return prev
+
+         return prev
+
+    
+    
 };
